d4b99a1ae6f685366522650166c1e8cfdf386112
Lotterygame44 <gs423427@gmail.com>
2026-03-31 18:03:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,57 +9,25 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 🎰 GAME SETTINGS
+// 🎰 GAME ENGINE SETTINGS
 let timer = 60; 
-let currentBets = {}; // { socketId: { name, number } }
+let currentBets = {}; // { socketId: { name, numbers: [] } }
 let totalPlayers = 0;
 
 setInterval(() => {
     timer--;
     if (timer < 0) {
-        // 🏆 6 UNIQUE LUCKY NUMBERS (1 to 25)
+        // 🏆 GENERATE 6 UNIQUE LUCKY NUMBERS (1-25)
         let luckyNumbers = [];
         while(luckyNumbers.length < 6) {
             let r = Math.floor(Math.random() * 25) + 1;
-            if(luckyNumbers.indexOf(r) === -1) luckyNumbers.push(r);
+            if(!luckyNumbers.includes(r)) luckyNumbers.push(r);
         }
         
-        // Winner Check Logic
+        // Winner Check: Agar user ke 6 picks mein se KOI BHI lucky numbers mein hai
         let winnersList = [];
         for (let id in currentBets) {
-            if (luckyNumbers.includes(currentBets[id].number)) {
-                winnersList.push(currentBets[id].name);
-            }
-        }
-
-        // Send Result to All
-        io.emit('gameResult', { 
-            numbers: luckyNumbers, // Array of 6 numbers
-            winners: winnersList, 
-            totalBets: Object.keys(currentBets).length 
-        });
-
-        timer = 60; 
-        currentBets = {}; 
-    }
-    io.emit('timer', timer);
-}, 1000);
-
-io.on('connection', (socket) => {
-    totalPlayers++;
-    io.emit('playerCount', totalPlayers);
-
-    socket.on('placeBet', (data) => {
-        currentBets[socket.id] = { name: data.name, number: data.number };
-        io.emit('updateLiveBets', Object.keys(currentBets).length);
-    });
-
-    socket.on('disconnect', () => {
-        totalPlayers--;
-        delete currentBets[socket.id];
-        io.emit('playerCount', totalPlayers);
-    });
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Jackpot Server Live: 1-25 Range | 6 Winners`));
+            const userPicks = currentBets[id].numbers;
+            const hasMatched = userPicks.some(num => luckyNumbers.includes(num));
+            
+            if (hasMatched) {
